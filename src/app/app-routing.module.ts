16f1b84b9ee781eb9f1fe6fa9778daa2e6a5b113
orodripi91008 <ogrodriguez@asesoftware.com>
2022-09-08 19:05:51 -0500

    Se agrega navbar-sidebar y nopagefound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { TemplateComponent } from './shared/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{ path: 'ingreso', component: IngresoComponent},
+  { path: 'informacion/:cliente', component: InformacionBasicaComponent},*/
+  { path: '', component:TemplateComponent},
+  { path: '**', pathMatch: 'full', component :NopagefoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { TemplateComponent } from './shared/template/template.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    TemplateComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
